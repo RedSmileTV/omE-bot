4b1cbb5ae2f050be09ca47cd5648d76b27dbdbc1
redlaptop <lelledefeo@gmail.com>
2024-02-20 18:24:10 +0100

    Commit 20/02/2024 18:24

--- src/main/java/de/redsmiletv/Commands.java
+++ src/main/java/de/redsmiletv/Commands.java
@@ -1,6 +1,28 @@
 package de.redsmiletv;
 
+
+import com.github.twitch4j.chat.TwitchChat;
+import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+
+
 public class Commands {
+    private TwitchChat chat;
+
+    public Commands(TwitchChat pChat) {
+        chat = pChat;
+    }
+    private void say() {
+        chat.getEventManager().onEvent(ChannelMessageEvent.class, event -> {
+
+
+            if (event.getMessage().toString().startsWith("!say")) {
+                chat.sendMessage("redsmiletv", event.getMessage().toString().substring(5));
+            }
+        });
+
+
+    }
+
 
 
 
--- src/main/java/de/redsmiletv/Main.java
+++ src/main/java/de/redsmiletv/Main.java
@@ -7,14 +7,67 @@ import com.github.twitch4j.TwitchClientBuilder;
 import com.github.twitch4j.chat.ITwitchChat;
 import com.github.twitch4j.chat.TwitchChat;
 import com.github.twitch4j.chat.TwitchChatBuilder;
+import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
 import com.github.twitch4j.chat.events.channel.IRCMessageEvent;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class Main {
     // Temp credentials, will be replaced with .env or .properties file
-    public static OAuth2Credential credential = new OAuth2Credential("twitch", "vbmggarmcie07qp72ap02hhvks8klw");
+    private static String TOKEN;
+    private static String CHANNEL_NAME;
+    public static TwitchClient twitchClient;
+    private static Commands commands;
     public static void main(String[] args) {
+        init();
+        run();
 
-        TwitchClient twitchClient = TwitchClientBuilder.builder()
+
+        // For listening messages like commands example !lol
+        //twitchClient.getClientHelper().enableStreamEventListener("redsmiletv");
+
+        //TwitchChat chat = twitchClient.getChat();
+
+        //chat.joinChannel("redsmiletv");
+        //chat.sendMessage("redsmiletv", "omE");
+
+//
+//        chat.getEventManager().onEvent(ChannelMessageEvent.class, event -> {
+//
+//            System.out.println(event.getUser() + ": " + event.getMessage().toString());
+//
+//            if (event.getMessage().toString().startsWith("!say")) {
+//                chat.sendMessage("redsmiletv", event.getMessage().toString().substring(5));
+//                //chat.sendMessage("redsmiletv", "omE");
+//            }
+//        });
+
+
+    }
+
+    private static void init() {
+        File file = new File("src/main/resources/.env");
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileReader(file));
+        } catch (IOException e) {
+            System.out.println("File not found");
+        }
+
+        TOKEN = properties.getProperty("TOKEN");
+        CHANNEL_NAME = properties.getProperty("CHANNEL_NAME");
+
+
+
+    }
+
+    public static void run() {
+        OAuth2Credential credential = new OAuth2Credential("twitch", TOKEN);
+        twitchClient = TwitchClientBuilder.builder()
                 .withDefaultAuthToken(credential)
                 .withDefaultEventHandler(SimpleEventHandler.class)
                 .withChatAccount(credential)
@@ -22,20 +75,14 @@ public class Main {
                 .withEnableHelix(true)
                 .build();
 
-        // For listening messages like commands example !lol
-        twitchClient.getClientHelper().enableStreamEventListener("redsmiletv");
 
-        TwitchChat chat = twitchClient.getChat();
+        commands = new Commands(twitchClient.getChat());
 
-        chat.joinChannel("redsmiletv");
-        //chat.sendMessage("redsmiletv", "omE");
+        twitchClient.getChat().joinChannel(CHANNEL_NAME);
+        twitchClient.getChat().sendMessage(CHANNEL_NAME, "omE");
+    }
 
-
-        chat.getEventManager().onEvent(IRCMessageEvent.class, event -> {
-            System.out.println(event.getMessage().toString());
-            if (event.getMessage().toString().contains("[omE]")) {
-                chat.sendMessage("redsmiletv", "omE");
-            }
-        });
+    public static TwitchClient getTwitchClient() {
+        return twitchClient;
     }
 }
